744133b298670aaace46418e505241f00da8cb10
SantiagoHM <santiago.hernandez@usantoto.edu.co>
2021-11-30 17:47:50 -0500

    models

--- Django_003_hardwareStore/products/models.py
+++ Django_003_hardwareStore/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "sellers"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=3000)
+    path = models.FileField(upload_to="")
+    price = models.FloatField()
+    discount = models.FloatField()
+    id_seller = models.ForeignKey(Seller, on_delete=CASCADE)
+
+    class Meta:
+        db_table = "products"
